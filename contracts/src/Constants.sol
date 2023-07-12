54c5f1fe5aa0b961e517186f75fddb5a167632fe
Kevin Park <kevin@fruitful.gg>
2023-07-12 14:44:23 +0800

    Initial implementation of Habit (#2)

---
+++ contracts/src/Constants.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.19;
+
+uint40 constant CHALLENGE_DURATION = 21 days;
+uint40 constant CHALLENGE_CHECKIN_RATE = 1 days;
+uint256 constant MINIMUM_WAGER = 5_000_000;
+
+address constant MOONBEAM_USDC_ADDR = 0x818ec0A7Fe18Ff94269904fCED6AE3DaE6d6dC0b;
