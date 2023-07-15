a96b937fdf0dc8894eb3e6b1e600d5d6c074bc5f
TBalo <tolu.balogun@maxfront.com>
2023-07-15 18:48:23 +0100

    added current-pledges, faq, footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HowTheAppFunctionsComponent } from './how-the-app-functions/how-the-app-functions.component';
+import { CurrentPledgesComponent } from './current-pledges/current-pledges.component';
+import { FAQComponent } from './faq/faq.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HowTheAppFunctionsComponent
+    HowTheAppFunctionsComponent,
+    CurrentPledgesComponent,
+    FAQComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
