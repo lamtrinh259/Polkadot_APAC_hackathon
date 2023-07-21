b495e77f46abb16d5dd8042ff4b4f8efd71acade
Mayank Yadav <ymayank97@gmail.com>
2023-07-21 16:14:23 -0400

    added DIARandom oracle code

--- contracts/src/Constants.sol
+++ contracts/src/Constants.sol
@@ -6,3 +6,5 @@ uint40 constant CHALLENGE_CHECKIN_RATE = 1 days;
 uint256 constant MINIMUM_WAGER = 5_000_000;
 
 address constant MOONBEAM_USDC_ADDR = 0x818ec0A7Fe18Ff94269904fCED6AE3DaE6d6dC0b;
+
+address constant oracle_address = 0x48d351aB7f8646239BbadE95c3Cc6de3eF4A6cec;
